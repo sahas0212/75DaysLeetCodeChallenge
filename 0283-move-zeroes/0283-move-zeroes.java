84c3eb4484d82322b643b03a58620d6a84fece09
SIRIGAM SAHAS REDDY <sahasreddy369@gmail.com>
2026-03-21 15:55:34 +0530

    Time: 2 ms (90.55%), Space: 47.3 MB (97.5%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,5 +1,27 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        // Arrays.sort(nums);
+        int n=nums.length,count=0,index=0;
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0)
+            {
+                count++;
+            }
+            else{
+                arr[index]=nums[i];
+                index++;
+            }
+        }
+        for(int i=index+1;i<n;i++)
+        {
+            arr[i]=0;
+        }
         
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=arr[i];
+        }
     }
 }
